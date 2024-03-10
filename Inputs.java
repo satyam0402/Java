1fda5766ae9b59103f9eebd209517696bc9fff61
Satyam Rajput <satyam04rajput02@gmail.com>
2024-03-10 17:31:01 -0500

    First Commit

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Inputs {
+    public static void main( String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Input : ");
+        int Roll = input.nextInt();
+        System.out.println("Roll No. : " + Roll);
+
+    }
+}
---
+++ Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+    public static void main(String[] args){
+        int Id = 60;
+        long largeInteger = 12312356;
+
+        char letter = 'r';
+
+        float marks = 43.50f;
+        double largeDecimalNumebrs = 456123.12345;
+
+        boolean check = false;
+
+
+
+    }
+}
