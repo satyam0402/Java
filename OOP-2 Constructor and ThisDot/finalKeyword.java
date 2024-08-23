e91f2280149f85036fa18d6ce215110c75b17c37
Satyam Rajput <91290831+satyam0402@users.noreply.github.com>
2024-08-23 17:39:43 -0500

    Add files via upload

---
+++ OOP-2 Constructor and ThisDot/finalKeyword.java
@@ -0,0 +1,20 @@
+//public class finalKeyword {
+//    public static void main(String[] args) {
+//        final A obj = new A("demo obj");
+//
+//        // WE CAN ASSIGN VALUES TO OBJ OF CLASS A BUT WE CAN NOT ASSIGN NEW CONSTRUCTOR VALUES
+//        // CAN
+//        obj.name = "demo obj name";
+//        //CANT assign a vale to final value object
+//        obj = new A ("demo obj name");
+//        // SO WHEN A NON-PRIMITIVE IS FINAL YOU CAN NOT RE ASSIGN IT.
+//
+//        System.out.println(obj.name);
+//    }
+//}
+//class A {
+//    String name;
+//    public A (String name){
+//        this.name = name;
+//    }
+//}
